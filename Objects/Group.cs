2368632cb1f23c029487428939576a75bc8f03a8
LenaKuchko <kuchkoel@gmail.com>
2017-05-26 11:42:59 -0700

    add class group

---
+++ Objects/Group.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+namespace AddressBook.Objects
+{
+  public class Group
+  {
+    private static List<Group> _groups = new List<Group> {};
+    private int _id;
+    private string _groupName;
+    private List<Contact> _groupContacts = new List<Contact> {};
+
+    public Group(string groupName)
+    {
+      _groupName = groupName;
+      _groups.Add(this);
+      _id = _groups.Count;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetGroupName()
+    {
+      return _groupName;
+    }
+    public static List<Group> GetAllGroups()
+    {
+      return _groups;
+    }
+    public List<Contact> GetAllGroupContacts()
+    {
+      return _groupContacts;
+    }
+  }
+}
