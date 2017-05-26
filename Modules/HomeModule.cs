2a99596d82824044176b0be92e1360eb277690c6
LenaKuchko <kuchkoel@gmail.com>
2017-05-26 09:19:48 -0700

    add functionality to add new contact and show it

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,18 @@ namespace AddressBook
   {
     public HomeModule()
     {
-
+      Get["/"] = _ => {
+        // Address address = new Address ("Oregon", "Portland", "5th Ave, 400");
+        // Contact newContact = new Contact("Epicodus", "8-888-888-88", address);
+        List<Contact> contacts = Contact.GetAllContacts();
+        return View["index.cshtml", contacts];
+      };
+      Get["/contacts/new"] = _ => View ["create_new_contact.cshtml"];
+      Post["/contacts/new"] = _ => {
+        Address address = new Address(Request.Form["state"], Request.Form["city"], Request.Form["street"]);
+        Contact newContact = new Contact(Request.Form["name"], Request.Form["phone"], address);
+        return View["show_new_contact.cshtml", newContact];
+      };
     }
   }
 }
