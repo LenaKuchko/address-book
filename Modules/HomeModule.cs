7251760ed6cb4fe2d9c49f45f3940e3dff5a9583
LenaKuchko <kuchkoel@gmail.com>
2017-05-26 08:10:47 -0700

    add initial folders and files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Nancy;
+using AddressBook.Objects;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
